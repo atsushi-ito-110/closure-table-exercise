30099030a7d2b52786cb643ef1cc6396b2bd8a15
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-05-26 00:43:47 +0900

    setting: routing, html setting when error

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'comments#index'
+  resources :comments do
+    member do
+      get :new_reply
+      post :reply
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
