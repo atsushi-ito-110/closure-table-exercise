dfaa33a85b1b9169d98bcda8b05c2e063e4f3971
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-05-28 17:59:28 +0900

    update: delete comment

--- app/models/closure.rb
+++ app/models/closure.rb
@@ -1,7 +1,4 @@
 class Closure < ApplicationRecord
   belongs_to :comment, class_name: 'Comment', foreign_key: 'ancestor_id'
-  # has_many :replies,
   belongs_to :reply,  class_name: 'Comment', foreign_key: 'descendant_id'
-
-  # belongs_to :comment, foreign_key: 'descendant_id'
 end
