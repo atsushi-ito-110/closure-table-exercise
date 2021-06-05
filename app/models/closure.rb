29c127756b94f34c8d543d672eed126f147b51bf
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-06-05 15:55:03 +0900

    fix: アソシエーションで不要なクラス指定

--- app/models/closure.rb
+++ app/models/closure.rb
@@ -1,4 +1,4 @@
 class Closure < ApplicationRecord
-  belongs_to :comment, class_name: 'Comment', foreign_key: 'ancestor_id'
+  belongs_to :comment, foreign_key: 'ancestor_id'
   belongs_to :reply,  class_name: 'Comment', foreign_key: 'descendant_id'
 end
